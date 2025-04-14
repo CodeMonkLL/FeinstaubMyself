6fce66a588764bc02c6db61ce113f4f316c030a1
Laura Miller <l.miller.2001@web.de>
2025-04-14 13:32:44 +0200

    Code for the migration and added migrate data in main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from modules import user_input;
 from modules import download_csv;
 from modules import delete_files;
+from modules import Migrate_csv_to_db;
 
+#download_csv.download_csv_month(user_input.UserInput.inputDate(),user_input.UserInput.inputSensor())
 
-download_csv.download_csv_month(user_input.UserInput.inputDate(),user_input.UserInput.inputSensor())
+Migrate_csv_to_db.migrate_csv_to_db()
--- modules/Migrate_csv_to_db.py
+++ modules/Migrate_csv_to_db.py
@@ -1,27 +1,41 @@
 from pathlib import Path
-import delete_files, sqlite3
-import download_csv, sqlite3
-import user_input, sqlite3
-
+import sqlite3
+import download_csv
 
 #Convert CSV data in a DB
 #Kade due CSV-Daten für das Jahr 2022 herunter und 
 #importiere sie in eine SQLite-Datenbank. Prüfe ggf. den korrekten
 #Import leerer EInträge als NULL-Werte.
-
+#Basispfad
 base_path = Path(__file__)
 
-full_path = base_path
+#SQLite-Datenbankverbindung
 conn = sqlite3.connect('feinstaub.db')
 cur = conn.cursor()
 
-cur.execute("SELECT timestamp, temperature FROM tem_hum")
+def migrate_csv_to_db():
+    #CSV-Dateien herunterladen
+    download_csv.download_csv_files()
 
-print("Zeit\t Temperatur")
-for ts, temp in cur:
-    print(ts, "\t", temp)
+    #CSV-Dateien in die SQLite-Datenbank importieren
+    for csv_file in base_path.glob("*.csv"):
+        issds = csv_file.name.__contains__("sds011")
+        with open(csv_file, 'r') as file:
+            next(file)  # Skip header row
+            if issds:
+                for line in file:
+                    data = line.strip().split(',')
+                    cur.execute("INSERT INTO sds011_metric (sensor_id,sensor_type,location,lat,lon,timestamp,P1,durP1,ratioP1,P2,durP2,ratioP2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]))
+            else:
+                for line in file:
+                    data = line.strip().split(',')
+                    cur.execute("INSERT INTO dht22_metric (sensor_id;sensor_type;location;lat;lon;timestamp;temperature;humidity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
+
+    conn.commit()
+    print("CSV-Dateien erfolgreich in die Datenbank importiert.")
 
 
+#Datenbankverbindung schließen
 conn.close()
 
 #Convert soll die csv-Dateien in die Feinstaub.db importieren/migrieren
