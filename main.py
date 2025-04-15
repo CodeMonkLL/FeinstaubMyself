a1b7ad1503bf2a19012d8fb2bbe8a371716c62cd
Laura Miller <l.miller.2001@web.de>
2025-04-15 11:56:44 +0200

    added comments and function extract_gz_to_db.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from modules import user_input;
+from modules import extract_gz_to_csv;
 from modules import Migrate_csv_to_db
 from modules import download_csv;
 from modules import delete_files;
 
+#löschen der alten csv und csv.gz dateien
 
+#runterladen der csv.gz dateien
 #download_csv.download_csv_month(user_input.UserInput.inputDate(),user_input.UserInput.inputSensor())
-# Teste die Funktion
+
+#Convertierung der csv.gz dateiten in csv dateien
+#extract_gz_to_csv.extract_gz_to_csv()
+#Starte den Import der CSV-Dateien in die Datenbank
 Migrate_csv_to_db.migrate_csv_to_db()
