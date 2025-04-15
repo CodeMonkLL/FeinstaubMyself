ee14998b936cf16321d33659ab2249d1a1b5b0a8
Laura Miller <l.miller.2001@web.de>
2025-04-15 11:58:29 +0200

    file optimized and adapted with import - Migrate_csv_to_db.py

--- modules/Migrate_csv_to_db.py
+++ modules/Migrate_csv_to_db.py
@@ -1,48 +1,36 @@
 from pathlib import Path
 import sqlite3
-from modules import download_csv
 
 #Basispfad
-base_path = Path(__file__).parent/'downloads'
-
-#SQLite-Datenbankverbindung
-conn = sqlite3.connect('feinstaub.db')
-cur = conn.cursor()
+base_path = Path(__file__).parent.parent/'csv' 
 
 def migrate_csv_to_db():
-    #CSV-Dateien herunterladen
-    download_csv.download_csv()
+    #SQLite-Datenbankverbindung
+    conn = sqlite3.connect('feinstaub.db')
+    cur = conn.cursor()
 
+    print("Starte den Import der CSV-Dateien in die Datenbank...")
     #CSV-Dateien in die SQLite-Datenbank importieren
+    #print(base_path)
     for csv_file in base_path.glob("*.csv"):
+        print( f"Importiere {csv_file.name} in die Datenbank...")
         issds = csv_file.name.__contains__("sds011")
         with open(csv_file, 'r') as file:
+            #print(file)
             next(file)  # Skip header row
             if issds:
                 for line in file:
-                    data = line.strip().split(',')
+                    print(line)
+                    data = line.strip().split(';')
                     cur.execute("INSERT INTO sds011_metric (sensor_id,sensor_type,location,lat,lon,timestamp,P1,durP1,ratioP1,P2,durP2,ratioP2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10], data[11]))
             else:
                 for line in file:
-                    data = line.strip().split(',')
-                    cur.execute("INSERT INTO dht22_metric (sensor_id;sensor_type;location;lat;lon;timestamp;temperature;humidity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]))
-
-# Debugging-Ausgabe
-def migrate_csv_to_db():
-    print(f"Base path: {base_path}")  
-    download_csv.download_csv()
-
-
-
-conn.commit()
-print("CSV-Dateien erfolgreich in die Datenbank importiert.")
-
-
-#Datenbankverbindung schließen
-conn.close()
-
-#Convert soll die csv-Dateien in die Feinstaub.db importieren/migrieren
-#Convert CSV data in a DB
-#Lade die CSV-Daten für das Jahr 2022 herunter und 
-#importiere sie in eine SQLite-Datenbank. Prüfe ggf. den korrekten
-#Import leerer EInträge als NULL-Werte.
+                    print(line)
+                    data = line.strip().split(';')
+                    #print("INSERT INTO dht22_metric (sensor_id,sensor_type,location,lat,lon,timestamp,temperature,humidity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (int(data[0]), data[1], data[2], float(data[3]), float(data[4]), data[5], float(data[6]), float(data[7])))
+                    cur.execute("INSERT INTO dht22_metric (sensor_id,sensor_type,location,lat,lon,timestamp,temperature,humidity) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (int(data[0]), data[1], data[2], float(data[3]), float(data[4]), data[5], float(data[6]), float(data[7])))
+    
+    conn.commit()                
+    print("CSV-Dateien erfolgreich in die Datenbank importiert.")
+    #Datenbankverbindung schließen
+    conn.close()
