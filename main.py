f37ac3bb0b8f25edd31d286be7581cba2595946a
Lukas Luemers <lukas.luemers@gdata.de>
2025-04-23 06:41:17 +0200

    merged max/min/avg into 1 data, deleted redundant code

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ from modules import user_input
 from modules import create_tables, migrate_csv_to_db
 from modules.extract_gz_to_csv import extract_gz_to_csv
 from modules.Migrate_csv_to_db import migrate_csv_to_db
-from modules import download_csv
-from modules import create_db;
-from pathlib import Path
+from modules.db_data_requests.data_user_input import display_data_func
 
 #löschen der alten csv und csv.gz dateien
 def main():
@@ -21,7 +19,8 @@ def main():
     #Starte den Import der CSV-Dateien in die Datenbank
     migrate_csv_to_db()
 
-   
+   #Zeige gewünschte Werte an
+    display_data_func()
 
 
 if __name__ == "__main__":
